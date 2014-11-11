2bb326493034ce33c7c5ff9bcccd49e16efb6bf9
Takahiro Nishimura <tkhr.nishimura@gmail.com>
2014-11-11 21:54:11 +0900

    AMI名をチェンジ

--- create_ami.rb
+++ create_ami.rb
@@ -10,7 +10,7 @@ region.each_value do |region|
   ec2 = AWS::EC2.new
   ec2.instances.each do |instance|
     target_ec2 = ec2.instances[instance.id]
-    image_name = "Auto Backup_#{Time.now.strftime("%F")}_#{target_ec2.tags["Name"]}"
+    image_name = "_Auto Backup_#{Time.now.strftime("%F")}_#{target_ec2.tags["Name"]}"
     puts "create AMI: #{target_ec2.tags["Name"]}"
     image = target_ec2.create_image(image_name, {no_reboot: true})
   end
