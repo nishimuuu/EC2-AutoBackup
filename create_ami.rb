9342c80a49d26d86c22cced2d7e0116ca6a1d04a
Takahiro Nishimura <tkhr.nishimura@gmail.com>
2014-11-12 12:57:50 +0900

    変数名変更, RDS自動バックアップ削除

--- create_ami.rb
+++ create_ami.rb
@@ -2,23 +2,18 @@ require 'aws-sdk'
 require 'yaml'
 
 dir = File.absolute_path('..',$0)
-region = YAML.load(File.read("#{dir}/region.yml"))
-region.each_value do |region|
+regions = YAML.load(File.read("#{dir}/region.yml"))
+regions.each_value do |aws_region|
   access_key =  YAML.load(File.read("#{dir}/access_key.yml"))
-  access_key.store('ec2_endpoint', region)
+  access_key.store('ec2_endpoint', aws_region)
   AWS.config(access_key)
+
   ec2 = AWS::EC2.new
+
   ec2.instances.each do |instance|
     target_ec2 = ec2.instances[instance.id]
     image_name = "_Auto Backup_#{Time.now.strftime("%F")}_#{target_ec2.tags["Name"]}"
     puts "create AMI: #{target_ec2.tags["Name"]}"
-    #image = target_ec2.create_image(image_name, {no_reboot: true})
-  end
-
-  rds = AWS::RDS.new
-  p rds.methods
-  rds.db_instances.each do |instance|
-    p instance.methods
-
+    target_ec2.create_image(image_name, {no_reboot: true})
   end
 end
